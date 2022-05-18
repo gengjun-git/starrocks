f2dbdbfc1b92c87cf22cddd79d9f1982ecad23fe
gengjun-git <gengjun@starrocks.com>
2022-05-18 13:19:49 +0800

    add

--- fe/fe-core/src/main/java/com/starrocks/http/rest/BDBBenchmarkAction.java
+++ fe/fe-core/src/main/java/com/starrocks/http/rest/BDBBenchmarkAction.java
@@ -9,6 +9,9 @@ import com.starrocks.http.BaseResponse;
 import com.starrocks.http.IllegalArgException;
 import io.netty.handler.codec.http.HttpMethod;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class BDBBenchmarkAction extends RestBaseAction {
 
     public BDBBenchmarkAction(ActionController controller) {
@@ -21,20 +24,40 @@ public class BDBBenchmarkAction extends RestBaseAction {
 
     @Override
     public void execute(BaseRequest request, BaseResponse response) {
-        long timeout = Long.parseLong(request.getSingleParameter("timeout")) * 1000L;
+        final long timeout = Long.parseLong(request.getSingleParameter("timeout")) * 1000L;
         int size = Integer.parseInt(request.getSingleParameter("msg_size"));
-        byte[] data = new byte[size];
-        long startTime = System.currentTimeMillis();
-        int cnt = 0;
-        while (System.currentTimeMillis() - startTime < timeout) {
-            Catalog.getCurrentCatalog().getEditLog().logBenchmark(data);
-            cnt++;
+        int concurrency = Integer.parseInt(request.getSingleParameter("concurrency"));
+        final byte[] data = new byte[size];
+        final long startTime = System.currentTimeMillis();
+        AtomicInteger cnt = new AtomicInteger(0);
+        CountDownLatch latch = new CountDownLatch(concurrency);
+        for (int i = 0; i < concurrency; i++) {
+            new Thread(() -> {
+                try {
+                    while (System.currentTimeMillis() - startTime < timeout) {
+                        Catalog.getCurrentCatalog().getEditLog().logBenchmark(data);
+                        cnt.getAndIncrement();
+                    }
+                } catch (Throwable t) {
+                    t.printStackTrace();
+                } finally {
+                    latch.countDown();
+                }
+            }).start();
         }
+
+        try {
+            latch.await();
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }
+
         response.getContent().append("write ")
-                .append(cnt)
+                .append(cnt.get())
                 .append(" logs(size: ").append(size).append(" bytes) in ")
                 .append(timeout / 1000L)
-                .append(" seconds\n");
+                .append(" seconds, concurrency is ")
+                .append(concurrency).append("\n");
         sendResult(request, response);
     }
 }
