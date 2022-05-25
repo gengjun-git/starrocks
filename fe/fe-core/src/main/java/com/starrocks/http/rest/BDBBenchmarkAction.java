c20180d1e8673a5fa4373f1dc2e4b81920e91635
gengjun-git <gengjun@starrocks.com>
2022-05-25 11:04:57 +0800

    add

--- fe/fe-core/src/main/java/com/starrocks/http/rest/BDBBenchmarkAction.java
+++ fe/fe-core/src/main/java/com/starrocks/http/rest/BDBBenchmarkAction.java
@@ -2,20 +2,34 @@
 
 package com.starrocks.http.rest;
 
+import com.sleepycat.bind.tuple.TupleBinding;
+import com.sleepycat.je.DatabaseEntry;
+import com.sleepycat.je.DatabaseException;
+import com.sleepycat.je.OperationStatus;
 import com.starrocks.catalog.Catalog;
+import com.starrocks.common.util.TimeUtils;
 import com.starrocks.http.ActionController;
 import com.starrocks.http.BaseRequest;
 import com.starrocks.http.BaseResponse;
 import com.starrocks.http.IllegalArgException;
+import com.starrocks.journal.bdbje.BDBEnvironment;
+import com.starrocks.journal.bdbje.BDBJEJournal;
+import com.starrocks.journal.bdbje.CloseSafeDatabase;
 import io.netty.handler.codec.http.HttpMethod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class BDBBenchmarkAction extends RestBaseAction {
+    public static final Logger LOG = LogManager.getLogger(BDBBenchmarkAction.class);
+
+    public AtomicLong key = new AtomicLong(0L);
 
     public BDBBenchmarkAction(ActionController controller) {
         super(controller);
@@ -27,6 +41,7 @@ public class BDBBenchmarkAction extends RestBaseAction {
 
     @Override
     public void execute(BaseRequest request, BaseResponse response) {
+        CloseSafeDatabase db = openDatabase();
         final long timeout = Long.parseLong(request.getSingleParameter("timeout")) * 1000L;
         int size = Integer.parseInt(request.getSingleParameter("msg_size"));
         int concurrency = Integer.parseInt(request.getSingleParameter("concurrency"));
@@ -40,9 +55,10 @@ public class BDBBenchmarkAction extends RestBaseAction {
                 try {
                     while (System.currentTimeMillis() - startTime < timeout) {
                         long startT = System.currentTimeMillis();
-                        Catalog.getCurrentCatalog().getEditLog().logBenchmark(data);
-                        latencyList.add(System.currentTimeMillis() - startT);
-                        cnt.getAndIncrement();
+                        if (write(db, data)) {
+                            latencyList.add(System.currentTimeMillis() - startT);
+                            cnt.getAndIncrement();
+                        }
                     }
                 } catch (Throwable t) {
                     t.printStackTrace();
@@ -63,7 +79,8 @@ public class BDBBenchmarkAction extends RestBaseAction {
 
         response.getContent().append("write ")
                 .append(cnt.get()).append(" logs(size: ").append(size).append(" bytes) in ")
-                .append(timeout / 1000L).append(" seconds, concurrency is ").append(concurrency).append("\n")
+                .append(timeout / 1000L).append(" seconds, concurrency is ").append(concurrency)
+                .append(" start time is ").append(TimeUtils.longToTimeString(startTime)).append("\n")
                 .append("qps is ").append(((double) (cnt.get())) / timeout * 1000).append("\n")
                 .append("mean latency is ").append(((double) sum) / cnt.get()).append(" ms\n")
                 .append("50% latency is ").append(latencyList.get((int) (cnt.get() * 0.5))).append(" ms\n")
@@ -78,7 +95,31 @@ public class BDBBenchmarkAction extends RestBaseAction {
         sendResult(request, response);
     }
 
-    public void openDatabase() {
+    public CloseSafeDatabase openDatabase() {
+        BDBEnvironment env = ((BDBJEJournal) Catalog.getCurrentCatalog().getEditLog().getJournal()).getBdbEnvironment();
+        return env.openDatabase("test");
+    }
 
+    public boolean write(CloseSafeDatabase db, byte[] data) {
+        Long id = key.getAndIncrement();
+        DatabaseEntry theKey = new DatabaseEntry();
+        TupleBinding<Long> idBinding = TupleBinding.getPrimitiveBinding(Long.class);
+        idBinding.objectToEntry(id, theKey);
+
+        DatabaseEntry theData = new DatabaseEntry(data);
+        boolean success = false;
+        for (int i = 0; i < 3; i++) {
+            try {
+                if (db.put(null, theKey, theData) == OperationStatus.SUCCESS) {
+                    success = true;
+                    break;
+                } else {
+                    LOG.warn("write bdb failed");
+                }
+            } catch (DatabaseException e) {
+                LOG.warn("write bdb failed", e);
+            }
+        }
+        return success;
     }
 }
